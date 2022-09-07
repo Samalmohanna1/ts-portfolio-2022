f7e8c2d7d394a890709804ebab87997d7b4f59ba
sumayahalmohanna <sumayahsaur@gmail.com>
2022-09-07 12:39:20 -0400

    ci: implement segment

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     GA_TRACKING_ID: process.env.GA_TRACKING_ID,
     HJID: process.env.HJID,
     HJSV: process.env.HJSV,
+    SEGMENT_WRITE_TOKEN: process.env.SEGMENT_WRITE_TOKEN,
   },
 }
 
