337a7d209b636771e390891d8a6ae73bae0378e8
sumayahalmohanna <sumayahsaur@gmail.com>
2022-09-08 18:40:22 -0400

    fix: og image tab use short internal path

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,7 @@ const Home: NextPage = () => {
         />
         <meta property='og:url' content='https://www.sam-almohanna.com' />
         <meta name='author' content='Sam Almohanna'></meta>
-        <meta
-          property='og:image'
-          content='../public/laser-eyes-profile-pic.gif'
-        />
+        <meta property='og:image' content='laser-eyes-profile-pic.gif' />
         <meta property='og:image:type' content='image/gif' />
         <meta property='og:image:width' content='400' />
         <meta property='og:image:height' content='300' />
@@ -28,15 +25,8 @@ const Home: NextPage = () => {
           property='og:image:alt'
           content='Animated profile picture of Sam.'
         />
-        <meta
-          name='twitter:card'
-          content='../public/laser-eyes-profile-pic.gif'
-        />
+        <meta name='twitter:card' content='laser-eyes-profile-pic.gif' />
         <title>Sam Almohanna | Creative Developer</title>
-        <meta
-          name='description'
-          content='Sam Almohanna - A creative web developer who is passionate about designing functional accessable digital products.'
-        />
         <link rel='icon' href='/favicon.svg' />
       </Head>
 
